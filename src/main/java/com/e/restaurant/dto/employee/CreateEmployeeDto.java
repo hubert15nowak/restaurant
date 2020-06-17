44a6062341e190647e2802faee0c81bc5a6381fe
Hubert <hubert15nowak@gmail.com>
2020-06-17 20:06:47 +0200

    create employee dto

---
+++ src/main/java/com/e/restaurant/dto/employee/CreateEmployeeDto.java
@@ -0,0 +1,30 @@
+package com.e.restaurant.dto.employee;
+
+import com.e.restaurant.database.entity.Employee;
+import com.e.restaurant.database.entity.User;
+import com.e.restaurant.database.enumerate.UserRole;
+import com.e.restaurant.security.PasswordConfig;
+
+public class CreateEmployeeDto {
+
+    public String login;
+    public String password;
+    public UserRole role;
+    public String name;
+    public String surname;
+    public String restaurant;
+
+    public Employee mapToEmployee() {
+        User user = new User();
+        user.setLogin(login);
+        user.setPassword(PasswordConfig.hashPassword(password));
+        user.setRole(role);
+        Employee employee = new Employee();
+        employee.setName(name);
+        employee.setName(surname);
+        employee.setUser(user);
+        return employee;
+    }
+
+
+}
