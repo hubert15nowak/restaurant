4c2eeafe2fd8bdb95d4f110a33b8648e9dfe6206
Hubert <hubert15nowak@gmail.com>
2020-06-17 17:11:21 +0200

    separate user from Employee

--- src/main/java/com/e/restaurant/database/entity/Employee.java
+++ src/main/java/com/e/restaurant/database/entity/Employee.java
@@ -1,27 +1,21 @@
 package com.e.restaurant.database.entity;
 
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Type;
+import com.e.restaurant.database.enumerate.EmployeeType;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Set;
 import java.util.UUID;
 
 @Entity
 public class Employee {
 
-    @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid", strategy = "uuid2")
-    @Type(type = "uuid-char")
-    @Column(length = 36, unique = true)
-    private UUID id;
+    @EmbeddedId
+    private EmployeeId id;
 
-    @Column(length = 45, nullable = false, unique = true)
-    private String login;
-
-    @Column(length = 60, nullable = false)
-    private String password;
+    @MapsId("id")
+    @OneToOne
+    private User user;
 
     @Column(length = 45)
     private String name;
@@ -34,4 +28,21 @@ public class Employee {
 
     @ManyToMany(mappedBy = "employees")
     private Set<Bill> bills;
+
+    public UUID getId() {
+        return user.getId();
+    }
+
+    public String getLogin() {
+        return user.getLogin();
+    }
+
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Embeddable
+    public static class EmployeeId implements Serializable {
+        UUID id;
+    }
 }
