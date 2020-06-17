ba0c159cfbb643e80982c3ac88eef895a27fdbe4
Hubert <hubert15nowak@gmail.com>
2020-06-17 17:16:34 +0200

    create UserDto

---
+++ src/main/java/com/e/restaurant/dto/UserDto.java
@@ -0,0 +1,21 @@
+package com.e.restaurant.dto;
+
+import com.e.restaurant.database.entity.User;
+import com.e.restaurant.database.enumerate.UserRole;
+
+public class UserDto {
+    public String login;
+    public String password;
+    public UserRole role;
+
+
+    public UserDto(String login, String password, UserRole role) {
+        this.login = login;
+        this.password = password;
+        this.role = role;
+    }
+
+    public static UserDto mapToDto(User user) {
+        return new UserDto(user.getLogin(), user.getPassword(), user.getRole());
+    }
+}
