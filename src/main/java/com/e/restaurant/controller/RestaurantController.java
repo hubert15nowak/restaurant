39231d90eca51483cb3555e55d452a92ae293541
Hubert <hubert15nowak@gmail.com>
2020-06-17 18:00:28 +0200

    create RestaurantController

---
+++ src/main/java/com/e/restaurant/controller/RestaurantController.java
@@ -0,0 +1,12 @@
+package com.e.restaurant.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/v1/restaurant")
+public class RestaurantController {
+
+
+
+}
