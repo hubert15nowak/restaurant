f3db4a585dd0b852c1c6490dc278087898d8d163
Hubert <hubert15nowak@gmail.com>
2020-06-17 20:04:02 +0200

    add getters and setters

--- src/main/java/com/e/restaurant/database/entity/Employee.java
+++ src/main/java/com/e/restaurant/database/entity/Employee.java
@@ -29,6 +29,55 @@ public class Employee {
     @ManyToMany(mappedBy = "employees")
     private Set<Bill> bills;
 
+    @Embeddable
+    public static class EmployeeId implements Serializable {
+        UUID id;
+    }
+
+    public void setId(EmployeeId id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public Set<Bill> getBills() {
+        return bills;
+    }
+
+    public void setBills(Set<Bill> bills) {
+        this.bills = bills;
+    }
+
     public UUID getId() {
         return user.getId();
     }
@@ -39,10 +88,5 @@ public class Employee {
 
     public String getPassword() {
         return user.getPassword();
-    }
-
-    @Embeddable
-    public static class EmployeeId implements Serializable {
-        UUID id;
     }
 }
