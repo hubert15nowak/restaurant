90f1963ae601e41f63373e451983b9be5a2f331b
Hubert <hubert15nowak@gmail.com>
2020-06-17 17:13:56 +0200

    move FeedstockPK

--- src/main/java/com/e/restaurant/database/repository/FeedStockRepository.java
+++ src/main/java/com/e/restaurant/database/repository/FeedStockRepository.java
@@ -1,8 +1,7 @@
 package com.e.restaurant.database.repository;
 
 import com.e.restaurant.database.entity.FeedStock;
-import com.e.restaurant.database.entity.FeedstockPK;
 import org.springframework.data.repository.CrudRepository;
 
-public interface FeedStockRepository extends CrudRepository<FeedStock, FeedstockPK> {
+public interface FeedStockRepository extends CrudRepository<FeedStock, FeedStock.FeedstockPK> {
 }
