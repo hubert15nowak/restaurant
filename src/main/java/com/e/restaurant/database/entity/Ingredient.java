3efebd92d5b17b859e913cc7b9a7581dde20ffbe
Hubert <hubert15nowak@gmail.com>
2020-06-17 17:14:16 +0200

    move IngredientPK

--- src/main/java/com/e/restaurant/database/entity/Ingredient.java
+++ src/main/java/com/e/restaurant/database/entity/Ingredient.java
@@ -1,7 +1,10 @@
 package com.e.restaurant.database.entity;
 
+import javax.persistence.Embeddable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
 
 @Entity
 public class Ingredient {
@@ -10,4 +13,35 @@ public class Ingredient {
     private IngredientPK pk;
 
     private double quantity;
+
+    @Embeddable
+    public static class IngredientPK implements Serializable {
+
+        @ManyToOne(optional = false)
+        private Unit unit;
+
+        @ManyToOne(optional = false)
+        private FeedStock feedStock;
+
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            IngredientPK that = (IngredientPK) o;
+
+            if (!unit.equals(that.unit)) return false;
+            return feedStock.equals(that.feedStock);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = unit.hashCode();
+            result = 31 * result + feedStock.hashCode();
+            return result;
+        }
+
+
+    }
 }
--- src/main/java/com/e/restaurant/database/repository/IngredientRepository.java
+++ src/main/java/com/e/restaurant/database/repository/IngredientRepository.java
@@ -1,8 +1,7 @@
 package com.e.restaurant.database.repository;
 
 import com.e.restaurant.database.entity.Ingredient;
-import com.e.restaurant.database.entity.IngredientPK;
 import org.springframework.data.repository.CrudRepository;
 
-public interface IngredientRepository extends CrudRepository<Ingredient, IngredientPK> {
+public interface IngredientRepository extends CrudRepository<Ingredient, Ingredient.IngredientPK> {
 }
