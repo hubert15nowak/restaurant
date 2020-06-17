0bc763844b7e35749829de2bab55d229421fb74b
Hubert <hubert15nowak@gmail.com>
2020-06-17 17:13:06 +0200

    move FeedStockPK

--- src/main/java/com/e/restaurant/database/entity/FeedStock.java
+++ src/main/java/com/e/restaurant/database/entity/FeedStock.java
@@ -1,13 +1,17 @@
 package com.e.restaurant.database.entity;
 
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 public class FeedStock {
 
     @EmbeddedId
     private FeedstockPK id;
+
+
+    @OneToOne
+    private Storeroom storeroom;
 
 
     public String getName() {
@@ -20,5 +24,54 @@ public class FeedStock {
 
     public Restaurant getRestaurant() {
         return id.getRestaurant();
+    }
+
+
+
+
+    @Embeddable
+    public static class FeedstockPK implements Serializable {
+
+        @Column(length = 45)
+        protected String name;
+        @Column(columnDefinition = "Decimal(7,2)")
+        protected double price;
+        @ManyToOne(optional = false)
+        private Restaurant restaurant;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            FeedstockPK that = (FeedstockPK) o;
+
+            if (Double.compare(that.price, price) != 0) return false;
+            if (!name.equals(that.name)) return false;
+            return restaurant.equals(that.restaurant);
+        }
+
+        @Override
+        public int hashCode() {
+            int result;
+            long temp;
+            result = name.hashCode();
+            temp = Double.doubleToLongBits(price);
+            result = 31 * result + (int) (temp ^ (temp >>> 32));
+            result = 31 * result + restaurant.hashCode();
+            return result;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+        public Restaurant getRestaurant() {
+            return restaurant;
+        }
     }
 }
