8a2c2115258823dc2ae56d2296f8a808ebaa51db
Hubert <hubert15nowak@gmail.com>
2020-06-17 17:18:56 +0200

    create UserService

---
+++ src/main/java/com/e/restaurant/service/UserService.java
@@ -0,0 +1,33 @@
+package com.e.restaurant.service;
+
+import com.e.restaurant.dao.UserDao;
+import com.e.restaurant.dto.CreateUserDto;
+import com.e.restaurant.dto.UserDto;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+public class UserService {
+
+    private final UserDao userDao;
+
+
+    public UserService(@Qualifier("userMysql") UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+
+    public void createUser(CreateUserDto user) throws DataIntegrityViolationException {
+        userDao.saveUser(user.mapToDao());
+    }
+
+    public Iterable<UserDto> getUsers() {
+        return StreamSupport.stream(userDao.getUsers().spliterator(), true)
+                .map(UserDto::mapToDto)
+                .collect(Collectors.toList());
+    }
+}
